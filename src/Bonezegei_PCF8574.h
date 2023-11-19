22d862e01b2d1c749f66ea992bd9f24ceb869b32
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-11-20 07:10:40 +0800

    Update Bonezegei_PCF8574.h

--- src/Bonezegei_PCF8574.h
+++ src/Bonezegei_PCF8574.h
@@ -1,1 +1,18 @@
+/*
+  This Library is written for PCF8574 I2C IO expander
+  Author: Bonezegei (Jofel Batutay)
+  Date:  November 2023 
+*/
+#ifndef _BONEZEGEI_PCF8574_H_
+#define _BONEZEGEI_PCF8574_H_
+#include <Arduino.h>
 
+class Bonezegei_PCF8574 {
+public:
+  Bonezegei_PCF8574();
+  Bonezegei_PCF8574(uint8_t addr);
+
+  char begin();
+};
+
+#endif
