726e8b698537e55f8511816fcd7b1a01cf739ad7
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-15 18:48:33 +0800

    Update Bonezegei_PCF8574.cpp

--- src/Bonezegei_PCF8574.cpp
+++ src/Bonezegei_PCF8574.cpp
@@ -1,7 +1,7 @@
 /*
   This Library is written for PCF8574 I2C IO expander
   Author: Bonezegei (Jofel Batutay)
-  Date:  November 2023
+  Date:  November 2023 
   Updated: Jan 15 2024
 */
 #include "Bonezegei_PCF8574.h"
@@ -14,18 +14,25 @@ Bonezegei_PCF8574::Bonezegei_PCF8574(uint8_t addr) {
 }
 
 char Bonezegei_PCF8574::begin() {
-  if(){
+  if (Wire.begin()) {
     return 1;
-  }
-  else{
+  } else {
     return 0;
   }
 }
 
-uint8_t Bonezegei_PCF8574::read(){
-  
+uint8_t Bonezegei_PCF8574::read() {
+  Wire.requestFrom(_addr | 0x01, 1);  
+
+  while (Wire.available()) {  
+    p_read = Wire.read();         
+  }
+  return p_read;
 }
 
-void Bonezegei_PCF8574::write(uint8_t data){
-  
+void Bonezegei_PCF8574::write(uint8_t data) {
+  Wire.beginTransmission(_addr);
+  p_write = data;
+  Wire.write(data);
+  Wire.endTransmission();
 }
