3561fe4b323db35a87c1c6057e33105e0da14e10
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-16 16:22:22 +0800

    Update ReadPort.ino

--- examples/ReadPort/ReadPort.ino
+++ examples/ReadPort/ReadPort.ino
@@ -1,1 +1,19 @@
+/*
+  Read Port
+  Author: Bonezegei (Jofel Batutay)
+  Date:  January 2024
+*/
 
+#include <Bonezegei_PCF8574.h>
+
+Bonezegei_PCF8574 io(0x27);
+
+void setup() {
+  Serial.begin(115200);
+  io.begin();
+}
+
+void loop() {
+  Serial.printf("Port:%x \n" , io.read());  //Read IO
+  delay(1000);
+}
